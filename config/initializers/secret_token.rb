9bd6269aa041cfc9773cd063ef67560206e68854
Szu <szu@Szu-Air.local>
2014-06-09 11:01:16 +0800

    First vision of RailsGirls APP2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubygirls::Application.config.secret_key_base = '64de9c1e426e4332c65f4e0bfd62e52e42519733604f25470f6798206c7d0c3e90bd72808898d1f7916fc44e78232dbbc93915a0515d8a53b7264e72a4ce715f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rubygirls::Application.config.session_store :cookie_store, key: '_rubygirls_session'
